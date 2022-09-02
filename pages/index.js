1fc1897adae75ac6e6541ffdda73febf8224463d
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-02 17:44:57 +0300

    chore: create drag system not done

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
+import React from "react";
 import Styles from '../styles/Home.module.scss'
+import { useState } from "react";
+import { motion, AnimateSharedLayout } from "framer-motion";
 
 function Builders() {
   const builderList = [
+    { slug: 'header' , name: 'Header', icon: 'H' },
+    { slug: 'header' , name: 'Header', icon: 'H' },
     { slug: 'header' , name: 'Header', icon: 'H' },
   ]
 
   return (
     builderList.map((item) => (
-      <div className={Styles.home__builder}>
+      <motion.div dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }} drag dragElastic={1} className={Styles.home__builder}>
         <div className={Styles['home__builder-icon']}>
           { item.icon }
         </div>
         <div className={Styles['home__builder-text']}>
           { item.name }
         </div>
-      </div>
+      </motion.div>
     ))
   )
 }
@@ -22,11 +27,13 @@ function Builders() {
 export default function Home() {
   return (
     <div className={Styles.home}>
-      <div className={Styles.home__list}>
-        <Builders />
-      </div>
-      <div className={Styles.home__form}>
-      </div>
+      <AnimateSharedLayout>
+        <div className={Styles.home__list}>
+          <Builders />
+        </div>
+        <div className={Styles.home__form}>
+        </div>
+      </AnimateSharedLayout>
     </div>
   )
 }
