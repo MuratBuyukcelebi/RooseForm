57dae853c1834315ac1e398154270d0fc47ad5b8
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-02 19:07:29 +0300

    chore: update

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
 import React from "react";
 import Styles from '../styles/Home.module.scss'
-import { useState } from "react";
 import { motion, AnimateSharedLayout } from "framer-motion";
 
 function Builders() {
   const builderList = [
-    { slug: 'header' , name: 'Header', icon: 'H' },
-    { slug: 'header' , name: 'Header', icon: 'H' },
-    { slug: 'header' , name: 'Header', icon: 'H' },
+    { slug: 'header' , name: 'Header' },
+    { slug: 'header' , name: 'Header' },
+    { slug: 'header' , name: 'Header' },
   ]
 
   return (
-    builderList.map((item) => (
-      <motion.div dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }} drag dragElastic={1} className={Styles.home__builder}>
+    builderList.map((item, index) => (
+      <motion.div key={index} dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }} drag dragElastic={1} className={Styles.home__builder}>
         <div className={Styles['home__builder-icon']}>
-          { item.icon }
+          { item.name.charAt(0) }
         </div>
         <div className={Styles['home__builder-text']}>
           { item.name }
         </div>
       </motion.div>
     ))
+  )
+}
+
+function FormElements() {
+  return (
+    <></>
   )
 }
 
@@ -32,6 +37,7 @@ export default function Home() {
           <Builders />
         </div>
         <div className={Styles.home__form}>
+          <FormElements />
         </div>
       </AnimateSharedLayout>
     </div>
