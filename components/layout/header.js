98c29f330e9a5571e16eff82457bbe0350520668
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-02 13:08:43 +0300

    feat: create  header

---
+++ components/layout/header.js
@@ -0,0 +1,12 @@
+import Styles from '../../styles/layout/Header.module.scss'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <div className={Styles.header}>
+      <a href="/">
+        <Image className={Styles.header__logo} src="/logo.svg" height={64} width={64} />
+      </a>
+    </div>
+  )
+}
--- layout/landing.js
+++ layout/landing.js
@@ -1,6 +1,9 @@
+import Header from '../components/layout/header'
+
 export default function Layout({ children }) {
   return (
     <>
+      <Header />
       <main>{children}</main>
     </>
   )
