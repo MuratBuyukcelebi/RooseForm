1209a5be1474f76e2a03200c90aff000134d165d
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-07 22:17:54 +0300

    chore: update

--- components/Dustbin.js
+++ components/Dustbin.js
@@ -1,20 +1,41 @@
-import { useDrop } from 'react-dnd'
-import { ItemTypes } from './ItemTypes.js'
-import Styles from "../styles/Home.module.scss";
 import React from "react";
+import Styles from "../styles/Home.module.scss";
+import { ItemTypes } from './ItemTypes.js'
+import { useDrop } from 'react-dnd'
+
+//Form Items
+import Header from "./form/header";
 
 export const Dustbin = () => {
-  const [_, drop] = useDrop(() => ({
+  const [{canDrop, isOver, getItem}, drop] = useDrop(() => ({
     accept: ItemTypes.BOX,
-    drop: (item, monitor) => {
-      console.log(monitor.getItem())
-    },
+    drop: (item, monitor) => {},
     collect: (monitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop(),
+      getItem: monitor.getItem(),
     }),
   }))
+  function FormList() {
+    if (isOver && canDrop && !!getItem?.slug) {
+      console.log('nice')
+    }
+
+    let list = [
+      { slug:'header', element: <Header /> }
+    ]
+    return (
+      list.map((item, key) =>
+        <div key={key}>
+          {item.element}
+        </div>
+      )
+    )
+  }
+
   return (
-    <div ref={drop} data-testid="dustbin" className={Styles.home__form} />
+    <div ref={drop} data-testid="dustbin" className={Styles.home__form}>
+      <FormList />
+    </div>
   )
 }
