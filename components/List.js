c86f506281333c8d07b36c8807d4161331de4320
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-16 19:33:53 +0300

    chore: update

--- components/List.js
+++ components/List.js
@@ -21,7 +21,12 @@ export const List = () => {
     { slug:'header', element: <Header /> },
     { slug:'test', element: <div>test</div> },
   ]
-  const [listTry, setListTry] = useState([{ slug:'header', element: <Header /> }])
+  const [listTry, setListTry] = useState([])
+
+  const deleteAction = (e) => {
+    const key = e.target.getAttribute("name")
+    console.log(key)
+  }
 
   return (
     <div ref={drop} data-testid="dustbin" className={Styles.home__form}>
@@ -31,7 +36,7 @@ export const List = () => {
             {item?.element}
           </div>
           <div className={Styles['home__form-action']}>
-            <button>
+            <button name={key} onClick={deleteAction}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/></svg>
             </button>
             <button>
