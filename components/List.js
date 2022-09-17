b10cdc0f7aa440dac287b0b1e7a531262f401d29
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-09-17 13:22:06 +0300

    chore: update action button (monke style)

--- components/List.js
+++ components/List.js
@@ -24,8 +24,7 @@ export const List = () => {
   const [listTry, setListTry] = useState([])
 
   const deleteAction = (e) => {
-    const key = e.target.getAttribute("name")
-    console.log(key)
+    e.target.parentElement.parentElement.remove()
   }
 
   return (
@@ -36,7 +35,7 @@ export const List = () => {
             {item?.element}
           </div>
           <div className={Styles['home__form-action']}>
-            <button name={key} onClick={deleteAction}>
+            <button onClick={deleteAction}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/></svg>
             </button>
             <button>
